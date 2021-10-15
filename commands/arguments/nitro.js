b1b0e86057821c134c339129be05e6c775a24b54
FanerTokuda <86337345+FanerTokuda@users.noreply.github.com>
2021-10-15 16:39:21 +0700

    update nitro

---
+++ commands/arguments/nitro.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: "nitro",
+    aliases: ['ni'],
+    delete: true,
+    
+    async execute (client, message, args) {
+        return message.channel.send(`
+        <:spotifylogo:883188529570676747> **THÔNG TIN VỀ NITRO** <:spotifylogo:883188529570676747> 
+
+        <a:nitro:883188390642745374> NITRO THÁNG <a:nitro:883188390642745374>
+        > <a:nitroboost:883188103987212288> Nitro Boost 170k VNĐ hoặc 8m5<a:owo:895874259132510269>/🥑**
+        > <a:nitroclassic:883188139437461524> Nitro Classic 90k VNĐ hoặc 4m5<a:owo:895874259132510269>/🥑**
+
+        <a:nitro:883188390642745374> NITRO NĂM <a:nitro:883188390642745374>
+        > <a:nitroboost:883188103987212288> Nitro Boost: 1tr5 VNĐ 
+        > <a:nitroclassic:883188139437461524> Nitro Classic 750k VNĐ
+        `);
+    }
+}
--- commands/misc/gift.js
+++ commands/misc/gift.js
@@ -34,12 +34,12 @@ module.exports = {
         if(rd > 960 && rd <= 980) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/spotify.png']});
         if(rd > 980 && rd <= 1000) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/netflix.png']});
         */
-        if(rd > 0 && rd <= 5000) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/mayman.png']});
-        if(rd > 5000 && rd <= 5010) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/nitroclassic.png']});
-        if(rd > 5010 && rd <= 5011) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/nitroboost.png']});
-        if(rd > 5011 && rd <= 8811) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/rd200.png']});
-        if(rd > 8811 && rd <= 8911) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/spotify.png']});
-        if(rd > 8911 && rd <= 10000) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/netflix.png']});
+        if(rd > 0 && rd <= 5300) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/mayman.png']});
+        if(rd > 5300 && rd <= 5310) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/nitroclassic.png']});
+        if(rd > 5310 && rd <= 5311) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/nitroboost.png']});
+        if(rd > 5311 && rd <= 8311) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/rd200.png']});
+        if(rd > 8311 && rd <= 8811) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/spotify.png']});
+        if(rd > 8811 && rd <= 10000) return message.reply({ allowedMentions: { repliedUser: false }, files: ['./image/netflix.png']});
     }
 }
 
