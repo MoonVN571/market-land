3d1cfdef8e8a9bc4b5e09eff6adef656a005f75d
MoonVN571 <hackerdmpicer3213212@gmail.com>
2021-10-15 16:17:26 +0700

    boost command, rename & change somethin

--- commands/arguments/method.js
+++ commands/arguments/method.js
@@ -5,16 +5,16 @@ module.exports = {
     
     async execute (client, message, args) {
         return message.channel.send(`
-        <:iuni:893342986459574343> CÁCH NHẬN NITRO <:iuni:893342986459574343>
-        <a:tick:883188266403242015>Loại 1: ADD CARD<a:tick:883188266403242015>
-        <a:muiten:898396608083939369> Full bảo hành
-        <a:muiten:898396608083939369> Không revoke
-        <a:muiten:898396608083939369> Hỗ trợ về đêm
+<:iuni:893342986459574343> CÁCH NHẬN NITRO <:iuni:893342986459574343>
+<a:tick:883188266403242015>Loại 1: ADD CARD<a:tick:883188266403242015>
+<a:muiten:898396608083939369> Full bảo hành
+<a:muiten:898396608083939369> Không revoke
+<a:muiten:898396608083939369> Hỗ trợ về đêm
 
-        <a:tick:883188266403242015>Loại 2: Link gift<a:tick:883188266403242015>
-        <a:muiten:898396608083939369> Không bảo hành
-        <a:muiten:898396608083939369> Có thể nhanh hoặc chậm
-        <a:muiten:898396608083939369> Thao tác dễ
+<a:tick:883188266403242015>Loại 2: Link gift<a:tick:883188266403242015>
+<a:muiten:898396608083939369> Không bảo hành
+<a:muiten:898396608083939369> Có thể nhanh hoặc chậm
+<a:muiten:898396608083939369> Thao tác dễ
 
         `);
     }
--- commands/arguments/netflix.js
+++ commands/arguments/netflix.js
@@ -5,12 +5,11 @@ module.exports = {
     
     async execute (client, message, args) {
         return message.channel.send(`
-             
-    <:netflixlogo:883008576086478889> BẢNG GIÁ NETFLIX <:netflixlogo:883008576086478889>
-    <a:tick:883188266403242015> NETFLIX PREMIUM 4K ULTRA HD <a:tick:883188266403242015>
-    > <a:tickxanh:898401565872893962>  Netflix 1 tháng - 1 người dùng - 40k VNĐ/1m8<a:owo:895874259132510269>/🥑 
-    > <a:tickxanh:898401565872893962>  Netflix 2 tháng - 1 người dùng - 80k VNĐ/3m6 <a:owo:895874259132510269>/🥑 
-    > <a:tickxanh:898401565872893962>  Mua từ 3 tháng trở lên: 35.000 x số tháng
+<:netflixlogo:883008576086478889> BẢNG GIÁ NETFLIX <:netflixlogo:883008576086478889>
+<a:tick:883188266403242015> NETFLIX PREMIUM 4K ULTRA HD <a:tick:883188266403242015>
+> <a:tickxanh:898401565872893962>  Netflix 1 tháng - 1 người dùng - 40k VNĐ/1m8<a:owo:895874259132510269>/🥑 
+> <a:tickxanh:898401565872893962>  Netflix 2 tháng - 1 người dùng - 80k VNĐ/3m6 <a:owo:895874259132510269>/🥑 
+> <a:tickxanh:898401565872893962>  Mua từ 3 tháng trở lên: 35.000 x số tháng
         `);
     }
 }
